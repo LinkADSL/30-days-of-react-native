c6c0ca9d656b61757ee4f19b06e04db329a64895
wangJiaXuan <lan_stone@foxmail.com>
2019-07-10 09:44:46 +0800

    ctrl shift l

--- view/day4.js
+++ view/day4.js
@@ -4,38 +4,38 @@
  */
 'use strict';
 
-import React,{ Component } from 'react';
-import { Image,StyleSheet,TouchableHighlight,View } from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, View} from 'react-native';
 
 
-export default class extends Component{
-  constructor() {
-    super();
-    this.state = {
-      show:false
-    };
-  }
+export default class extends Component {
+    constructor() {
+        super();
+        this.state = {
+            show: false
+        };
+    }
 
-  _onImgPress() {
-    this.setState({
-      show:false
-    })
-    this.setState({
-      show:true
-    })
-  }
+    _onImgPress() {
+        this.setState({
+            show: false
+        })
+        this.setState({
+            show: true
+        })
+    }
 
-  render() {
-    return(
-      <View style={{marginTop:100, alignItems:"center"}}>
-      </View>
-    )
-  }
+    render() {
+        return (
+            <View style={{marginTop: 100, alignItems: "center"}}>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  img:{
-    height: 200,
-    width: 300
-  },
+    img: {
+        height: 200,
+        width: 300
+    },
 });
